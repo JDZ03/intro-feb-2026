28c99c33d1ab1981e8bf832e8a882774a5fce509
Jack Dziubinski <jackdziubinski@gmail.com>
2026-02-10 14:19:20 -0500

    Update

--- src/week1/MuddiestMomentSolution/AppHost/AppHost.cs
+++ src/week1/MuddiestMomentSolution/AppHost/AppHost.cs
@@ -1,6 +1,21 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var mmApi = builder.AddProject<Projects.MuddiestMoment_Api>("mm-api");
+// Infastructure - "backing services" - what is my app going to need in the anviornment in which it runs.
+// - database - postgres (great support for relational data (rows and columns) and for documents (like mongodb))
+// - identity provider (later)
+
+var postGres = builder.AddPostgres("db-server")
+    .WithLifetime(ContainerLifetime.Persistent); // We have in prod a postgres server
+
+// We are going to need a database on that server for the API
+var mmDb = postGres.AddDatabase("db-mm");
+
+
+
+var mmApi = builder.AddProject<Projects.MuddiestMoment_Api>("mm-api")
+    .WithReference(mmDb)
+    .WaitFor(mmDb);
+
 var gateway = builder.AddProject<Projects.Gateway_Api>("gateway")
     .WithReference(mmApi)
     .WaitFor(mmApi);
